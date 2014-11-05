030e568039b84b31ba49d739e1de9d4f3e1a6bf2
Ailsa Chiu <chiu.ailsa@gmail.com>
2014-11-04 22:38:49 -0800

    completed nearest-neighbor, need to check results; TODO: fixed queries

--- hw2/hw2.java
+++ hw2/hw2.java
@@ -39,7 +39,7 @@ public class hw2 {
 				System.out.println("window tramstop");
 				windowQuery("tramstops", x0, y0, xf, yf);
 			}
-		}
+		} // end of window
 
 		else if(queryType.equals("within")) {
 			if(args.length < 3) {
@@ -54,6 +54,53 @@ public class hw2 {
 				System.exit(0);
 			}
 			withinQuery(student_id,distance);
+		} // end of within
+
+		else if(queryType.equals("nearest-neighbor")) {
+			if(args.length < 4) {
+				System.out.println("Use: java hw2 nearest-neighbor [neighbor object type] [object id] [number of neighbors]");
+				System.exit(0);
+			}
+
+			String neighborObject = args[1].toLowerCase();
+			String id = args[2].toLowerCase();
+			int numNeighbors = Integer.parseInt(args[3]);
+
+			if(!neighborObject.equals("student") && !neighborObject.equals("building") && !neighborObject.equals("tramstop")) {
+				System.out.println("Invalid object; choose from: student, building, tramstop");
+				System.exit(0);
+			}
+			if(numNeighbors < 1) {
+				System.out.println("No neighbors to find");
+				System.exit(0);
+			}
+
+			System.out.println(neighborObject + " " + id + " " + numNeighbors);
+			nearestNeighborsQuery(neighborObject, id, numNeighbors);
+		} // end of nearest-neighbor
+
+		else if(queryType.equals("fixed")) {
+			if(args.length < 2) {
+				System.out.println("Use: java hw2 fixed [query number]");
+				System.exit(0);
+			}
+
+			int queryNumber = Integer.parseInt(args[1]);
+			if(queryNumber == 1) {
+				queryOne();
+			}
+			else if(queryNumber == 2) {
+
+			}
+			else if(queryNumber == 3) {
+
+			}
+			else if(queryNumber == 4) {
+
+			}
+			else if(queryNumber == 5) {
+
+			}
 		}
 
 		System.out.println("Done");
@@ -100,8 +147,7 @@ public class hw2 {
         		System.out.println();
     		}
 
-		    rs.close();
-		    
+		    rs.close();		    
 		    stmt.close();
 		    conn.close();
 
@@ -129,6 +175,9 @@ public class hw2 {
 			ResultSet rs = stmt.executeQuery(query);
 			if(!rs.next()) {
 				System.out.println("Invalid student id");
+				rs.close();
+				stmt.close();
+				conn.close();
 				System.exit(0);
 			}
 
@@ -159,5 +208,87 @@ public class hw2 {
 		    System.out.println("SQLState: " + ex.getSQLState());
 		    System.out.println("VendorError: " + ex.getErrorCode());
 		}	
+	}
+
+	public static void nearestNeighborsQuery(String neighborObject, String object_id, int numNeighbors) {
+		Connection conn = null;
+		String tablename = "";
+		String id = "";
+		String geom = "";
+
+		if(neighborObject.equals("student")) {
+			tablename = "students";
+			id = "student_id";
+			geom = "location";
+		}
+		else if(neighborObject.equals("tramstop")) {
+			tablename = "tramstops";
+			id = "tramstop_id";
+			geom = "Buffer(location,radius)";
+		}
+		else if(neighborObject.equals("building")) {
+			tablename = "buildings";
+			id = "building_id";
+			geom = "geom";
+		}
+
+		try {
+		    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs585_hw2","java","password");
+
+		    if(conn == null) {
+		    	System.out.println("Failed connection");
+		    	System.exit(0);
+		    }
+
+		    Statement stmt = conn.createStatement();
+		    String query = "select * from " + tablename + " where " + id + " = '" + object_id + "';";
+			ResultSet rs = stmt.executeQuery(query);
+			if(!rs.next()) {
+				System.out.println("Invalid " + neighborObject + " id");
+				rs.close();
+				stmt.close();
+				conn.close();
+				System.exit(0);
+			}
+
+			/*
+			set @geom = (select geom from buildings where building_id = 'b3');
+			select building_id
+			from buildings
+			where ST_Within(geom, Buffer(@geom,1000)) and building_id != 'b3'
+			order by ST_Distance(geom,@geom)
+			limit 5;
+			*/	
+			stmt.executeUpdate("set @geom = (select " + geom + " from " + tablename + " where " + id + " = '" + object_id + "');");
+			query = "select " + id + " from " + tablename + " where ST_Within(" + geom + ", Buffer(@geom,1000)) and " + id + " != '" + object_id + "' " 
+				+ "order by ST_Distance(" + geom + ", @geom) limit " + numNeighbors + ";";
+
+			rs = stmt.executeQuery(query);
+		    ResultSetMetaData rsmd = rs.getMetaData();
+		    int columnsNumber = rsmd.getColumnCount();
+		    while (rs.next()) {
+		        for (int i = 1; i <= columnsNumber; i++) {
+		            if (i > 1) System.out.print(",  ");
+		            String columnValue = rs.getString(i);
+		            System.out.print(columnValue); 
+		        }
+        		System.out.println();
+    		}
+
+		    rs.close(); 
+		    stmt.close();
+		    conn.close();
+
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+
+	public static void queryOne() {
+		// Find the ids of all the students and buildings cover by tram stops: t2ohe and t6ssl.
+
 	}
 }
